ca46f6df62eaf266e30020fe97e8c59bfd5d50f8
TrevorBrowning <trevryanbrowning@gmail.com>
2025-01-02 20:37:11 -0500

    Added fps refresh

--- main.py
+++ main.py
@@ -11,14 +11,21 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
 
+    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
+            
             if event.type == pygame.QUIT:
                 return  # Exit the program
 
-        screen.fill((0, 0, 0))  # Fill the display with black
+        screen.fill((0, 0, 0))  
 
-        pygame.display.flip()  # Refresh the screen to show the changes
+        pygame.display.flip() 
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
